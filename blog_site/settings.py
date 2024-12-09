42fe15140bf3564a117cd2b1a51fee142ba05fc7
kimi.casamina <kimi.casamina@gmail.com>
2024-12-09 21:19:09 +0800

    static folders

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# define the path for the static folder
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
