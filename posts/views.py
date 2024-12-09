3c37b1b54cee1fcc9cda3226eb6da6fb3231b0c1
kimi.casamina <kimi.casamina@gmail.com>
2024-12-09 23:04:45 +0800

    added posts_page.html with slugs, used named urls

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 def posts_list(request):
     # return HttpResponse('hello world, this is the homepage')
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/posts_page.html', {'post': post})
