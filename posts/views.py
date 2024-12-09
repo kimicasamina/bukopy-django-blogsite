cffd74d49aebf10a45f4234512197f39bc6b3fa9
kimi.casamina <kimi.casamina@gmail.com>
2024-12-09 22:32:01 +0800

    created a superuser for admin, and included the all the posts object on the views to access it from posts_list.html

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 def posts_list(request):
     # return HttpResponse('hello world, this is the homepage')
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
