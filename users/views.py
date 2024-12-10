799da016c4230ea9203a47a9b6afcfdcaae1231b
kimi.casamina <kimi.casamina@gmail.com>
2024-12-10 14:43:33 +0800

    created a registration form and validation

--- users/views.py
+++ users/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm 
 
 def register_view(request):
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        # validate form, save to the database and redirect to another url
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else: 
+        # create an empty form instead and send the form errors
+        form = UserCreationForm()
+    return render(request, 'users/register.html', { "form": form })
