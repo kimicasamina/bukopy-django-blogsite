5d89df4eb1d0927f0e3214514805a2f9f41c3308
kimi.casamina <kimi.casamina@gmail.com>
2024-12-14 22:05:02 +0800

    updated settings

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Build the project
-echo "Building the project..."
+echo "Installing dependencies..."
 python3.10 -m pip install -r requirements.txt
 
-echo "Make Migrations..."
+echo "Making Migrations..."
 python3.10 manage.py makemigrations --noinput
 python3.10 manage.py migrate --noinput
 
-echo "Collect Static..."
+echo "Collecting Static Files..."
 python3.10 manage.py collectstatic --noinput --clear
